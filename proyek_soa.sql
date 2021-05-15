f978553ed0bb259cc100166f0881fffb43150d71
LastMoon88 <72486337+LastMoon88@users.noreply.github.com>
2021-05-15 22:08:09 +0800

    updated DB

--- proyek_soa.sql
+++ proyek_soa.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2021 at 10:59 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: May 15, 2021 at 04:06 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,12 +30,23 @@ USE `proyek_soa`;
 --
 
 DROP TABLE IF EXISTS `favorite`;
-CREATE TABLE IF NOT EXISTS `favorite` (
-  `id_favorite` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+CREATE TABLE `favorite` (
+  `id_favorite` int(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `id_game` int(11) NOT NULL,
   `tgl_favorite` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `favorite`
+--
+
+INSERT INTO `favorite` (`id_favorite`, `email`, `id_game`, `tgl_favorite`) VALUES
+(1, 'ss', 11, '2021-05-15 13:32:29'),
+(2, 'ss', 11, '2021-05-15 13:33:12'),
+(3, 'dwd', 33, '0000-00-00 00:00:00'),
+(4, 'gg', 44, '2021-05-15 13:42:18'),
+(8, 'dicky@gmail.com', 1, '2021-05-15 13:56:07');
 
 -- --------------------------------------------------------
 
@@ -45,9 +55,9 @@ CREATE TABLE IF NOT EXISTS `favorite` (
 --
 
 DROP TABLE IF EXISTS `history`;
-CREATE TABLE IF NOT EXISTS `history` (
+CREATE TABLE `history` (
   `id_history` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `id_game` int(11) NOT NULL,
   `tgl_history` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `deskripsi_history` varchar(255) NOT NULL
@@ -60,9 +70,9 @@ CREATE TABLE IF NOT EXISTS `history` (
 --
 
 DROP TABLE IF EXISTS `review`;
-CREATE TABLE IF NOT EXISTS `review` (
+CREATE TABLE `review` (
   `id_review` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `id_game` int(11) NOT NULL,
   `rating` int(11) NOT NULL,
   `review` text NOT NULL
@@ -75,16 +85,72 @@ CREATE TABLE IF NOT EXISTS `review` (
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `nama_user` varchar(255) NOT NULL,
   `jenis_user` varchar(1) NOT NULL,
   `gambar_profile` text NOT NULL,
   `saldo_user` int(11) NOT NULL,
-  `api_hit` int(11) NOT NULL,
-  PRIMARY KEY (`email`)
+  `api_hit` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`email`, `password`, `nama_user`, `jenis_user`, `gambar_profile`, `saldo_user`, `api_hit`) VALUES
+('dicky@gmail.com', 'ggwp', 'dicky', 'P', './uploadsdicky.jpg', 999699, 835);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `favorite`
+--
+ALTER TABLE `favorite`
+  ADD PRIMARY KEY (`id_favorite`);
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`id_history`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`id_review`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `favorite`
+--
+ALTER TABLE `favorite`
+  MODIFY `id_favorite` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `history`
+--
+ALTER TABLE `history`
+  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
