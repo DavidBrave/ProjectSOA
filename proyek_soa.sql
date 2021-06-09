ae3daee44d79271f9f44552d54387c8350e49695
LastMoon88 <72486337+LastMoon88@users.noreply.github.com>
2021-06-09 20:39:40 +0800

    db baru

--- proyek_soa.sql
+++ proyek_soa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2021 at 04:06 PM
+-- Generation Time: Jun 09, 2021 at 02:38 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -32,7 +32,7 @@ USE `proyek_soa`;
 DROP TABLE IF EXISTS `favorite`;
 CREATE TABLE `favorite` (
   `id_favorite` int(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `api_key` varchar(255) NOT NULL,
   `id_game` int(11) NOT NULL,
   `tgl_favorite` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -41,12 +41,18 @@ CREATE TABLE `favorite` (
 -- Dumping data for table `favorite`
 --
 
-INSERT INTO `favorite` (`id_favorite`, `email`, `id_game`, `tgl_favorite`) VALUES
+INSERT INTO `favorite` (`id_favorite`, `api_key`, `id_game`, `tgl_favorite`) VALUES
 (1, 'ss', 11, '2021-05-15 13:32:29'),
 (2, 'ss', 11, '2021-05-15 13:33:12'),
 (3, 'dwd', 33, '0000-00-00 00:00:00'),
 (4, 'gg', 44, '2021-05-15 13:42:18'),
-(8, 'dicky@gmail.com', 1, '2021-05-15 13:56:07');
+(8, 'dicky@gmail.com', 1, '2021-05-15 13:56:07'),
+(11, 'dicky@gmail.com', 0, '2021-05-16 12:49:57'),
+(12, 'dicky@gmail.com', 0, '2021-05-16 12:51:31'),
+(13, 'dicky@gmail.com', 0, '2021-05-16 12:54:40'),
+(14, 'dicky@gmail.com', 3498, '2021-05-27 06:58:30'),
+(15, 'dicky@gmail.com', 3498, '2021-05-27 07:14:40'),
+(16, 'dicky@gmail.com', 1, '2021-05-27 07:15:32');
 
 -- --------------------------------------------------------
 
@@ -57,7 +63,7 @@ INSERT INTO `favorite` (`id_favorite`, `email`, `id_game`, `tgl_favorite`) VALUE
 DROP TABLE IF EXISTS `history`;
 CREATE TABLE `history` (
   `id_history` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `api_key` varchar(255) NOT NULL,
   `id_game` int(11) NOT NULL,
   `tgl_history` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `deskripsi_history` varchar(255) NOT NULL
@@ -72,11 +78,22 @@ CREATE TABLE `history` (
 DROP TABLE IF EXISTS `review`;
 CREATE TABLE `review` (
   `id_review` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `api_key` varchar(255) NOT NULL,
   `id_game` int(11) NOT NULL,
   `rating` int(11) NOT NULL,
   `review` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `review`
+--
+
+INSERT INTO `review` (`id_review`, `api_key`, `id_game`, `rating`, `review`) VALUES
+(1, 'dicky@gmail.com', 1, 80, 'NaN'),
+(2, 'dicky@gmail.com', 1, 80, 'mantul'),
+(3, 'dicky@gmail.com', 1, 80, 'mantul'),
+(4, 'dicky@gmail.com', 1, 80, 'mantul'),
+(5, 'dicky@gmail.com', 1, 80, 'mantul2');
 
 -- --------------------------------------------------------
 
@@ -86,6 +103,7 @@ CREATE TABLE `review` (
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
+  `api_key` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `nama_user` varchar(255) NOT NULL,
@@ -99,8 +117,8 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`email`, `password`, `nama_user`, `jenis_user`, `gambar_profile`, `saldo_user`, `api_hit`) VALUES
-('dicky@gmail.com', 'ggwp', 'dicky', 'P', './uploadsdicky.jpg', 999699, 835);
+INSERT INTO `user` (`api_key`, `email`, `password`, `nama_user`, `jenis_user`, `gambar_profile`, `saldo_user`, `api_hit`) VALUES
+('', 'dicky@gmail.com', 'ggwp', 'dicky', 'P', './uploadsdicky.jpg', 998499, 710);
 
 --
 -- Indexes for dumped tables
@@ -128,7 +146,7 @@ ALTER TABLE `review`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`api_key`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -138,7 +156,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `favorite`
 --
 ALTER TABLE `favorite`
-  MODIFY `id_favorite` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_favorite` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `history`
@@ -150,7 +168,7 @@ ALTER TABLE `history`
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
